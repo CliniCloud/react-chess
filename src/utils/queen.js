fc96c5e2fca7c1d3c04b6bcbafd1b6670bc559c9
aquilesds <aquilesds@clinicloud.com>
2018-06-15 12:19:19 +1000

    Adding queen functions

--- src/utils/queen.js
+++ src/utils/queen.js
@@ -1,25 +1,30 @@
-const bishop = require('./bishop')
-const rook = require('./rook')
+const bishop = require('./bishop');
+const rook = require('./rook');
 
+/**
+ * Get next movements and attacks from queen
+ * @param {array} pieces all current pieces
+ * @param {object} piece decoded piece
+ * @param {object} threateningPos enemyś threatening king position
+ */
  const getOptions = (pieces, piece, threateningPos) =>{
-    let nextMovements = []
-    let attacks = []
+    let nextMovements = [];
+    let attacks = [];
 
-    const bishopResult = bishop.getOptions(pieces, piece, threateningPos)
-    nextMovements = nextMovements.concat(bishopResult.nextMovements)
-    attacks = attacks.concat(bishopResult.attacks)
+    const bishopResult = bishop.getOptions(pieces, piece, threateningPos);
+    nextMovements = nextMovements.concat(bishopResult.nextMovements);
+    attacks = attacks.concat(bishopResult.attacks);
 
-    const rookResult = rook.getOptions(pieces, piece, threateningPos)
-    nextMovements = nextMovements.concat(rookResult.nextMovements)
-    attacks = attacks.concat(rookResult.attacks)
+    const rookResult = rook.getOptions(pieces, piece, threateningPos);
+    nextMovements = nextMovements.concat(rookResult.nextMovements);
+    attacks = attacks.concat(rookResult.attacks);
 
     return {
         nextMovements,
         attacks
-    }
-}
-
+    };
+};
 
 module.exports = {
     getOptions
-}
+};
