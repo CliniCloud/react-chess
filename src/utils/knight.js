099b07f43f96358cbe65ab7c3cb939db90f83a47
aquilesds <aquilesds@clinicloud.com>
2018-06-15 12:17:52 +1000

    Adding knight functions and tests

--- src/utils/knight.js
+++ src/utils/knight.js
@@ -1,59 +1,114 @@
 const general = require('./general')
-const decode = require('../decode')
 
-const isRangeMatch = (range, x, y) => {
-    for(const item of range){
-        if(item.x === x && item.y === y){
-            return true
+const allowedMovements = [{
+  x: 1,
+  y: 2,
+}, {
+  x: -1,
+  y: 2,
+}, {
+  x: 2,
+  y: 1,
+}, {
+  x: -2,
+  y: 1,
+}, {
+  x: 2,
+  y: -1,
+}, {
+  x: -2,
+  y: -1,
+}, {
+  x: 1,
+  y: -2,
+}, {
+  x: -1,
+  y: -2,
+}];
+
+/**
+ * validate if x and y position are a allowed knight movement
+ * @param {arrat} range list of possible knight movements
+ * @param {int} x x board position
+ * @param {int} y y board position
+ */
+const isRangeMatch = (x, y) => !!allowedMovements.find(item => item.x === x && item.y === y);
+
+/**
+ * Create movement and attack based in board position
+ * @param {array} pieces all current pieces
+ * @param {object} piece decoded piece
+ * @param {int} xPos knight x position
+ */
+const getAttacktNextMov = (pieces, piece, xPos) => {
+  const column = general.getColumn(xPos);
+  const attacks = [];
+  const nextMovements = [];
+  if (column) {
+    for (let row = -2; row < 3; row++) {
+      const yPos = piece.y + row;
+      if (yPos - piece.y !== 0 && general.isBetweenRange(yPos) && isRangeMatch(xPos - piece.x, yPos - piece.y)) {
+        const squarePiece = general.findPieceAtPosition(pieces, `${column}${yPos + 1}`);
+        if (squarePiece && !general.isPiecesFromSameTeam(squarePiece, piece)) {
+          attacks.push({
+            x: xPos,
+            y: yPos,
+          });
+        } else {
+          nextMovements.push({
+            x: xPos,
+            y: yPos,
+          });
         }
+      }
+    }
+  }
+  return {
+    attacks,
+    nextMovements,
+  };
+};
+
+/**
+ * Get next movements and attacks 
+ * @param {array} pieces all current pieces
+ * @param {object} piece decoded piece
+ * @param {object} threateningPos enemy's position threatening king
+ */
+const getOptions = function (pieces, piece, threateningPos) {
+  let nextMovements = [];
+  let attacks = [];
+  for (let col = -2; col < 3; col++) {
+    const movAtt = getAttacktNextMov(pieces, piece, piece.x - col);
+    if (movAtt && movAtt.attacks.length) {
+      attacks = attacks.concat(movAtt.attacks);
     }
 
-    return false
-}
-
-const getOptions = function(pieces, piece, threateningPos) {
-    const position = piece.x ? piece : decode.fromPieceDecl(piece.notation)
-    const allowedMovements = [{x:1,y:2}, {x:-1,y:2}, {x:2,y:1}, {x:-2,y:1}, {x:2,y:-1}, {x:-2,y:-1}, {x:1,y:-2}, {x:-1,y:-2}]
-    const nextMovements = []
-    let attacks = []
-
-    for (let col = -2; col < 3; col++) {
-        const newX = position.x - col
-        const column = general.getColumn(newX)
-        if(column){
-            for (let row = -2; row < 3; row++) {
-                const newY = position.y - row
-                if(general.isValidRange(newY)){
-                    if(isRangeMatch(allowedMovements, newX - position.x, newY - position.y)){
-                        const squarePiece = general.findPieceAtPosition(pieces, `${column}${newY+1}`)
-                        if(squarePiece){
-                            if(!general.isPiecesFromSameTeam(squarePiece, piece)){
-                                attacks.push({x:newX, y:newY})
-                            }
-                        }else{
-                            nextMovements.push({x:newX, y:newY})
-                        }
-                    }
-                }
-            }
-        }
+    if (movAtt && movAtt.nextMovements.length) {
+      nextMovements = nextMovements.concat(movAtt.nextMovements);
     }
+  }
 
-    if(threateningPos){
-        const threathPos = decode.fromPieceDecl(threateningPos)
-        const newAttacks = []
-        for(const attack of attacks){
-            if(attack.x === threathPos.x && attack.y === threathPos.y){
-                newAttacks.push(attack)
-                break 
-            }
-        }
-        attacks = newAttacks
-    }
+  const threatenedAttacks = general.getThreateningAttack(attacks, threateningPos);
+  if (threatenedAttacks) {
+    attacks = threatenedAttacks;
+  }
 
-    return {nextMovements, attacks}
+  return {
+    nextMovements,
+    attacks,
+  };
+};
+
+let API = {
+  getOptions,
+};
+
+if (general.isTestEnv()) {
+  API = Object.assign({}, API, {
+    getAttacktNextMov,
+    isRangeMatch,
+  });
 }
 
-module.exports = {
-    getOptions
-}
+module.exports = API;
---
+++ test/utils/knight.test.js
@@ -0,0 +1,201 @@
+const knight = require('../../src/utils/knight');
+const decode = require('../../src/decode');
+const mockedData = require('./knightMockData');
+
+
+describe('knight', () => {
+    describe('isRangeMatch', () => {
+        test('should return true', () => {
+            expect(knight.isRangeMatch(-2, -1)).toBe(true);
+            expect(knight.isRangeMatch(-2, 1)).toBe(true);
+            expect(knight.isRangeMatch(-1, -2)).toBe(true);
+            expect(knight.isRangeMatch(-1, 2)).toBe(true);
+            expect(knight.isRangeMatch(1, 2)).toBe(true);
+            expect(knight.isRangeMatch(1, -2)).toBe(true);
+            expect(knight.isRangeMatch(2, 1)).toBe(true);
+            expect(knight.isRangeMatch(2, -1)).toBe(true);
+        });
+
+        test('should return false', () => {
+            expect(knight.isRangeMatch(-2, -2)).toBe(false);
+            expect(knight.isRangeMatch(-2, 2)).toBe(false);
+            expect(knight.isRangeMatch(-1, -1)).toBe(false);
+            expect(knight.isRangeMatch(-1, 1)).toBe(false);
+            expect(knight.isRangeMatch(1, -1)).toBe(false);
+            expect(knight.isRangeMatch(1, 1)).toBe(false);
+            expect(knight.isRangeMatch(2, -2)).toBe(false);
+            expect(knight.isRangeMatch(2, 2)).toBe(false);
+        });
+    });
+
+    describe('getAttacktNextMov movements', () => {
+        test('should create two left movements in extreme left', () => {
+            const piece = decode.fromPieceDecl(mockedData.movementsLineUp[2]);
+            const xPos = piece.x - 2;
+            const result = knight.getAttacktNextMov(mockedData.movementsLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(2);
+            expect(result).toEqual(mockedData.twoLeftMovesExtreme);
+        })
+
+        test('should create two left movements in middle left', () => {
+            const piece = decode.fromPieceDecl(mockedData.movementsLineUp[2]);
+            const xPos = piece.x - 1;
+            const result = knight.getAttacktNextMov(mockedData.movementsLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(2);
+            expect(result).toEqual(mockedData.twoLeftMovesMiddle);
+        });
+
+        test('should create two right movements in middle right', () => {
+            const piece = decode.fromPieceDecl(mockedData.movementsLineUp[2]);
+            const xPos = piece.x + 1;
+            const result = knight.getAttacktNextMov(mockedData.movementsLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(2);
+            expect(result).toEqual(mockedData.twoRightMovesMiddle);
+        });
+
+        test('should create two right movements in extreme right', () => {
+            const piece = decode.fromPieceDecl(mockedData.movementsLineUp[2]);
+            const xPos = piece.x + 2;
+            const result = knight.getAttacktNextMov(mockedData.movementsLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(2);
+            expect(result).toEqual(mockedData.twoRightMovesExtreme);
+        });
+
+        test('should not create any movement in the middle', () => {
+            const piece = decode.fromPieceDecl(mockedData.movementsLineUp[2]);
+            const result = knight.getAttacktNextMov(mockedData.movementsLineUp, piece, piece.x);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(0);
+        });
+
+        test('should not create a movement in an inexistent column', () => {
+
+            const piece = decode.fromPieceDecl(mockedData.getAttacktNextMovLineup1[2]);
+            const xPos = piece.x - 2;
+            const result = knight.getAttacktNextMov(mockedData.getAttacktNextMovLineup1, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(0);
+        });
+
+        test('should not create a movement in an inexistent up row', () => {
+            const piece = decode.fromPieceDecl(mockedData.getAttacktNextMovLineup2[2]);
+            const xPos = piece.x - 2;
+            const result = knight.getAttacktNextMov(mockedData.getAttacktNextMovLineup2, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(1);
+            expect(result).toEqual(mockedData.notCreateMovInexUPRow);
+        });
+
+        test('should not create a movement in an inexistent down row', () => {
+            const piece = decode.fromPieceDecl(mockedData.getAttacktNextMovLineup3[2]);
+            const xPos = piece.x - 2;
+            const result = knight.getAttacktNextMov(mockedData.getAttacktNextMovLineup3, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(1);
+            expect(result).toEqual(mockedData.notCreateMovInexDownRow);
+        });
+    });
+
+    describe('getAttacktNextMov attacks', () => {
+        test('should create two left attacks in the extreme left', () => {
+            const piece = decode.fromPieceDecl(mockedData.attacksLineUp[2]);
+            const xPos = piece.x - 2;
+            const result = knight.getAttacktNextMov(mockedData.attacksLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(2);
+            expect(result.nextMovements).toHaveLength(0);
+            expect(result).toEqual(mockedData.twoAttacksExtremeLeft);
+        });
+
+        test('should create two left attacks in the middle left', () => {
+            const piece = decode.fromPieceDecl(mockedData.attacksLineUp[2]);
+            const xPos = piece.x - 1;
+            const result = knight.getAttacktNextMov(mockedData.attacksLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(2);
+            expect(result.nextMovements).toHaveLength(0);
+            expect(result).toEqual(mockedData.twoAttacksMiddleLeft);
+        });
+
+        test('should create two right attacks in the middle right', () => {
+            const piece = decode.fromPieceDecl(mockedData.attacksLineUp[2]);
+            const xPos = piece.x + 1;
+            const result = knight.getAttacktNextMov(mockedData.attacksLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(2);
+            expect(result.nextMovements).toHaveLength(0);
+            expect(result).toEqual(mockedData.twoAttacksMiddleRight);
+        });
+
+        test('should create two right attacks in the extreme right', () => {
+            const piece = decode.fromPieceDecl(mockedData.attacksLineUp[2]);
+            const xPos = piece.x + 2;
+            const result = knight.getAttacktNextMov(mockedData.attacksLineUp, piece, xPos);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(2);
+            expect(result.nextMovements).toHaveLength(0);
+            expect(result).toEqual(mockedData.twoAttacksExtremeRight);
+        });
+
+        test('should not create any attacks in the middle', () => {
+            const piece = decode.fromPieceDecl(mockedData.attacksLineUp[2]);
+            const result = knight.getAttacktNextMov(mockedData.attacksLineUp, piece, piece.x);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(0);
+            expect(result.nextMovements).toHaveLength(0);
+        });
+    });
+
+    describe('getOptions', () => {
+        test('should return eight attacks and no movements', () => {
+            const piece = decode.fromPieceDecl(mockedData.attacksLineUp[2]);
+            const result = knight.getOptions(mockedData.attacksLineUp, piece);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(8);
+            expect(result.nextMovements).toHaveLength(0);
+            expect(result).toEqual(mockedData.eightAttacks);
+        });
+
+        test('should return five attacks and three movements', () => {
+            const piece = decode.fromPieceDecl(mockedData.getOptions5Att3Movs[2]);
+            const result = knight.getOptions(mockedData.getOptions5Att3Movs, piece);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(5);
+            expect(result.nextMovements).toHaveLength(3);
+            expect(result).toEqual(mockedData.fiveAttacksThreeMovs);
+        });
+
+        test('should return four attacks and two movements', () => {
+            const piece = decode.fromPieceDecl(mockedData.getOptions4Att2Movs[2]);
+            const result = knight.getOptions(mockedData.getOptions4Att2Movs, piece);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(4);
+            expect(result.nextMovements).toHaveLength(2);
+            expect(result).toEqual(mockedData.fourAttacksTwoMovs);
+        });
+
+        test('should return one attack to king threatening pos', () => {
+            const piece = decode.fromPieceDecl(mockedData.getOptions1Att2KingThreat[2]);
+            const result = knight.getOptions(
+                mockedData.getOptions1Att2KingThreat,
+                piece,
+                mockedData.getOptions1Att2KingThreat[7]);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(1);
+            expect(result.nextMovements).toHaveLength(0);
+            expect(result).toEqual(mockedData.oneAttackNoMovs);
+        });
+    });
+});
