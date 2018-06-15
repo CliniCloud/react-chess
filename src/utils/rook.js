9642c613906431f6c8341280ce5578fb92ef0b7e
aquilesds <aquilesds@clinicloud.com>
2018-06-15 12:16:58 +1000

    Adding rook funcions and tests

--- src/utils/rook.js
+++ src/utils/rook.js
@@ -1,88 +1,247 @@
 const general = require('./general')
-const decode = require('../decode')
 
-const getAttacktNextMov = (pieces, piece, endedDirection, col, row, type) => {
-    let nextMov = null
-    let attack = null
-    let newEndedDirection
-    const column = general.getColumn(col)
-    if(column){
-        const squarePiece = general.findPieceAtPosition(pieces, `${column}${row+1}`)
+/**
+ * Generate movement or attack for a board position
+ * @param {array} pieces all current pieces
+ * @param {object} piece decoded piece
+ * @param {*} endDir flag container to check if movements for especific side finished
+ */
+const getAttacktNextMov = (pieces, piece, endDir) => {
+    const column = general.getColumn(piece.col)
+    if (column) {
+        const squarePiece = general.findPieceAtPosition(pieces, `${column}${piece.row+1}`)
         if (squarePiece) {
-            newEndedDirection = Object.assign(endedDirection, {[type]:true})
             if (!general.isPiecesFromSameTeam(squarePiece, piece)) {
-                attack = {
-                    x: col,
-                    y: row
+                return {
+                    attack: {
+                        x: piece.col,
+                        y: piece.row
+                    },
+
+                    endDir: Object.assign(
+                        {}, 
+                        endDir, 
+                        { [piece.side]: true }
+                    ),
                 }
             }
-        } else {
-            nextMov = {
-                x: col,
-                y: row
+
+            return {
+                endDir: Object.assign(
+                    {},
+                    endDir, 
+                    { [piece.side]: true }
+                ),
             }
+        } 
+        
+        return {
+            nextMov: {
+                x: piece.col,
+                y: piece.row
+            },
+            endDir,
         }
     }
 
-    return {
-        nextMov,
-        attack,
-        endedDirection: newEndedDirection ? newEndedDirection : endedDirection
-    }
+    return null;
 }
 
-const generateRookPos = (pieces, piece, position, endedDirection, col, row, type) => {
-    let shouldCall = false
-    if ((position.x === col || position.y === row) && (position.x !== col || position.y !== row)) {
+/**
+ * Validate x or y position are the same of the rook
+ * @param {object} pos position object
+ */
+const isValidPosition = pos => pos.x === pos.col || pos.y === pos.row 
 
-        if (type === 'left' && !endedDirection.left && col < position.x) {
-            shouldCall = true
-        } else if (type === 'right' && !endedDirection.right && col > position.x) {
-            shouldCall = true
-        } else if (type === 'top' && !endedDirection.top && row > position.y) {
-            shouldCall = true
-        } else if (type === 'down' && !endedDirection.down && row < position.y) {
-            shouldCall = true
-        }
+/**
+ * Validate if x and y are inside of rook left movements and attacks
+ * @param {object} pos position object
+ * @param {object} endeDir flag container to check if movements for especific side finished
+ */
+const isLeftPosition = (pos, endeDir) => pos.side === 'left' && !endeDir.left && pos.col < pos.x
 
-        if (shouldCall) {
-            return getAttacktNextMov(pieces, piece, endedDirection, col, row, type)
+/**
+ * Validate if x and y are inside of rook right movements and attacks
+ * @param {object} pos position object
+ * @param {object} endeDir flag container to check if movements for especific side finished
+ */
+const isRightPosition = (pos, endeDir) => pos.side === 'right' && !endeDir.right && pos.col > pos.x
+
+/**
+ * Validate if x and y are inside of rook up movements and attacks
+ * @param {object} pos position object
+ * @param {object} endeDir flag container to check if movements for especific side finished
+ */
+const isUpPosition = (pos, endeDir) => pos.side === 'top' && !endeDir.top && pos.row > pos.y
+
+/**
+ * Validate if x and y are inside of rook down movements and attacks
+ * @param {object} pos position object
+ * @param {object} endeDir flag container to check if movements for especific side finished
+ */
+const isDownPosition = (pos, endeDir) => pos.side === 'down' && !endeDir.down && pos.row < pos.y
+
+/**
+ * Validate if x and y belongs to any side movements and return it
+ * @param {array} pieces all current pieces
+ * @param {object} piece decoded piece
+ * @param {object} endeDir flag container to check if movements for especific side finished
+ */
+const generateRookPos = (pieces, piece, endeDir) => {
+    if (isValidPosition(piece)) {
+        if(isLeftPosition(piece, endeDir) || isRightPosition(piece, endeDir) || isUpPosition(piece, endeDir) || isDownPosition(piece, endeDir)){
+            return getAttacktNextMov(pieces, piece, endeDir)
         }
     }
 
     return null
 }
 
-const createRookResult = (result, rookResults) => {
-    if (rookResults) {
-        if (rookResults.nextMov) {
-            result.nextMovements.push(rookResults.nextMov)
-        }
-
-        if (rookResults.attack) {
-            result.attacks.push(rookResults.attack)
-        }
-
-        if (rookResults.nexMovAfterKing) {
-            result.nexMovsAfterKing.push(rookResults.nexMovAfterKing)
-        }
-
-        if (rookResults.nextAttackAfterKing) {
-            result.nextAttacksAfterKing.push(rookResults.nextAttackAfterKing)
+/**
+ * Get movements and attacks to left
+ * @param {array} pieces all current pieces
+ * @param {object} piece  decoded piece 
+ * @param {object} endedDirection flag container to check if movements for especific side finished
+ */
+const getLeftPosition = (pieces, piece, endedDirection) => {
+    let endDir = Object.assign({}, endedDirection)
+    const attacks = []
+    const movs = []
+    const newPiece = Object.assign({}, piece, {
+        side: 'left'
+    })
+    for (let col = newPiece.x; col > -1; col--) {
+        newPiece.col = col
+        for (let row = 0; row < 8; row++) {
+            newPiece.row = row
+            const rookResult = generateRookPos(pieces, newPiece, endDir)
+            endDir = rookResult && rookResult.endDir ? rookResult.endDir : endDir
+            if (rookResult && rookResult.attack) {
+                attacks.push(rookResult.attack)
+            } else if (rookResult && rookResult.nextMov) {
+                movs.push(rookResult.nextMov)
+            }
         }
     }
 
-    return result
+    return {
+        endDir,
+        attacks,
+        movs
+    }
 }
 
-const getOptions = (pieces, piece, threateningPos) => {
-    const position = piece.x  ? piece : decode.fromPieceDecl(piece.notation)
-    const result = {
-        nexMovsAfterKing: [],
-        nextAttacksAfterKing: [],
-        nextMovements: [],
-        attacks: []
+/**
+ * Get movements and attacks to right
+ * @param {array} pieces all current pieces
+ * @param {object} piece  decoded piece 
+ * @param {object} endedDirection flag container to check if movements for especific side finished
+ */
+const getRightPosition = (pieces, piece, endedDirection) => {
+    let endDir = Object.assign({}, endedDirection)
+    const attacks = []
+    const movs = []
+    const newPiece = Object.assign({}, piece, {
+        side: 'right'
+    })
+    for (let col = newPiece.x; col < 8; col++) {
+        newPiece.col = col
+        for (let row = 0; row < 8; row++) {
+            newPiece.row = row
+            const rookResult = generateRookPos(pieces, newPiece, endDir)
+            endDir = rookResult && rookResult.endDir ? rookResult.endDir : endDir
+            if (rookResult && rookResult.attack) {
+                attacks.push(rookResult.attack)
+            } else if (rookResult && rookResult.nextMov) {
+                movs.push(rookResult.nextMov)
+            }
+        }
     }
+
+    return {
+        endDir,
+        attacks,
+        movs
+    }
+}
+
+/**
+ * Get movements and attacks to down
+ * @param {array} pieces all current pieces
+ * @param {object} piece  decoded piece 
+ * @param {object} endedDirection flag container to check if movements for especific side finished
+ */
+const getDownPosition = (pieces, piece, endedDirection) => {
+    let endDir = Object.assign({}, endedDirection)
+    const attacks = []
+    const movs = []
+    const newPiece = Object.assign({}, piece, {
+        side: 'down'
+    })
+    for (let col = 0; col < 8; col++) {
+        newPiece.col = col
+        for (let row = newPiece.y; row > -1; row--) {
+            newPiece.row = row
+            const rookResult = generateRookPos(pieces, newPiece, endDir)
+            endDir = rookResult && rookResult.endDir ? rookResult.endDir : endDir
+            if (rookResult && rookResult.attack) {
+                attacks.push(rookResult.attack)
+            } else if (rookResult && rookResult.nextMov) {
+                movs.push(rookResult.nextMov)
+            }
+        }
+    }
+
+    return {
+        endDir,
+        attacks,
+        movs
+    }
+}
+
+/**
+ * Get movements and attacks to up
+ * @param {array} pieces all current pieces
+ * @param {object} piece  decoded piece 
+ * @param {object} endedDirection flag container to check if movements for especific side finished
+ */
+const getUpPosition =(pieces, piece, endedDirection) => {
+    let endDir = Object.assign({}, endedDirection)
+    const attacks = []
+    const movs = []
+    const newPiece = Object.assign({}, piece, {
+        side: 'top'
+    })
+    for (let col = 0; col < 8; col++) {
+        newPiece.col = col
+        for (let row = newPiece.y; row < 8; row++) {
+            newPiece.row = row
+            const rookResult = generateRookPos(pieces, newPiece, endDir)
+            endDir = rookResult && rookResult.endDir ? rookResult.endDir : endDir
+            if (rookResult && rookResult.attack) {
+                attacks.push(rookResult.attack)
+            } else if (rookResult && rookResult.nextMov) {
+                movs.push(rookResult.nextMov)
+            }
+        }
+    }
+
+    return {
+        endDir,
+        attacks,
+        movs
+    }
+}
+
+/**
+ * Create next movements and attacks for rook when its king is threatened or not
+ * @param {array} pieces all current pieces
+ * @param {object} piece  decoded piece
+ * @param {object} threateningPos king's threatening enemy's pos
+ */
+const getOptions = (pieces, piece, threateningPos) => {
+    let nextMovements = []
+    let attacks = []
 
     let endedDirection = {
         left: false,
@@ -91,53 +250,64 @@ const getOptions = (pieces, piece, threateningPos) => {
         down: false
     }
 
-    for (let col = position.x; col > -1; col--) {
-        for (let row = 0; row < 8; row++) {
-            const rookResult = generateRookPos(pieces, piece, position, endedDirection, col, row, 'left')
-            endedDirection = rookResult && rookResult.endedDirection ? rookResult.endedDirection :  endedDirection
-            createRookResult(result, rookResult)
-        }
+    const leftResult = getLeftPosition(pieces, piece, endedDirection)
+    if (leftResult) {
+        nextMovements = nextMovements.concat(leftResult.movs)
+        attacks = attacks.concat(leftResult.attacks)
+        endedDirection = leftResult.endDir
+    }
+    
+    const rightResult = getRightPosition(pieces, piece, endedDirection)
+    if (rightResult) {
+        nextMovements = nextMovements.concat(rightResult.movs)
+        attacks = attacks.concat(rightResult.attacks)
+        endedDirection = rightResult.endDir
+    }
+    
+    const downResult = getDownPosition(pieces, piece, endedDirection)
+    if (downResult) {
+        nextMovements = nextMovements.concat(downResult.movs)
+        attacks = attacks.concat(downResult.attacks)
+        endedDirection = downResult.endDir
+    }
+    
+    const upResult = getUpPosition(pieces, piece, endedDirection)
+    if (upResult) {
+        nextMovements = nextMovements.concat(upResult.movs)
+        attacks = attacks.concat(upResult.attacks)
+        endedDirection = upResult.endDir
     }
 
-    for (let col = position.x; col < 8; col++) {
-        for (let row = 0; row < 8; row++) {
-            const rookResult = generateRookPos(pieces, piece, position, endedDirection, col, row, 'right')
-            endedDirection = rookResult && rookResult.endedDirection ? rookResult.endedDirection :  endedDirection
-            createRookResult(result, rookResult)
-        }
+    const threateningAttacks = general.getThreateningAttack(attacks, threateningPos)
+    if(threateningAttacks){
+        attacks = threateningAttacks
     }
-
-
-    for (let col = 0; col < 8; col++) {
-        for (let row = position.y; row > -1; row--) {
-            const rookResult = generateRookPos(pieces, piece, position, endedDirection, col, row, 'down')
-            endedDirection = rookResult && rookResult.endedDirection ? rookResult.endedDirection :  endedDirection
-            createRookResult(result, rookResult)
-        }
+    
+    return {
+        nextMovements,
+        attacks
     }
-
-    for (let col = 0; col < 8; col++) {
-        for (let row = position.y; row < 8; row++) {
-            const rookResult = generateRookPos(pieces, piece, position, endedDirection, col, row, 'top')
-            endedDirection = rookResult && rookResult.endedDirection ? rookResult.endedDirection :  endedDirection
-            createRookResult(result, rookResult)
-        }
-    }
-
-    if (threateningPos) {
-        const threathPos = decode.fromPieceDecl(threateningPos)
-        const newAttacks = []
-        for (const attack of result.attacks) {
-            if (attack.x === threathPos.x && attack.y === threathPos.y) {
-                newAttacks.push(attack)
-                break
-            }
-        }
-        result.attacks = newAttacks
-    }
-    return result
 }
 
-module.exports = {
+
+let API = {
     getOptions
 }
+
+if (general.isTestEnv()) {
+    API = Object.assign({}, API, {
+        isValidPosition,
+        isLeftPosition,
+        isRightPosition,
+        isUpPosition,
+        isDownPosition,
+        generateRookPos,
+        getLeftPosition,
+        getRightPosition,
+        getDownPosition,
+        getUpPosition,
+        getAttacktNextMov
+    })
+}
+
+module.exports = API
---
+++ test/utils/rook.test.js
@@ -0,0 +1,343 @@
+const rook = require('../../src/utils/rook');
+const decode = require('../../src/decode');
+const mockedData = require('./rookMockData');
+
+
+describe('Rook', () => {
+    let endDir = {};
+
+    describe("getAttacktNextMov", () => {
+        beforeEach(() => {
+            endDir = {
+                left: false,
+                right: false,
+                top: false,
+                down: false
+            };
+        });
+
+        test("should returns one down attack", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 2;
+            piece.col = 2;
+            piece.side = 'down';
+            const result = rook.getAttacktNextMov(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneDownAttack);
+        })
+
+        test("should returns one right attack", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 5;
+            piece.col = 4;
+            piece.side = 'right';
+            const result = rook.getAttacktNextMov(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneRightAttack);
+        });
+
+        test("should returns one left attack", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 5;
+            piece.col = 0;
+            piece.side = 'left';
+            const result = rook.getAttacktNextMov(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneLeftAttack);
+        });
+
+        test("should returns one top attack", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 7;
+            piece.col = 2;
+            piece.side = 'top';
+            const result = rook.getAttacktNextMov(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneTopAttack);
+        });
+
+        test("should returns one down movement", () => {
+            const piece = decode.fromPieceDecl(mockedData.movementLineUp[1]);
+            piece.row = 2;
+            piece.col = 2;
+            piece.side = 'down';
+            const result = rook.getAttacktNextMov(mockedData.movementLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneDownMovement);
+        });
+
+        test("should returns one right movement", () => {
+            const piece = decode.fromPieceDecl(mockedData.movementLineUp[1]);
+            piece.row = 5;
+            piece.col = 4;
+            piece.side = 'right';
+            const result = rook.getAttacktNextMov(mockedData.movementLineUp, piece, endDir);
+
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneRightMovement);
+        })
+
+        test("should returns one left movement", () => {
+            const piece = decode.fromPieceDecl(mockedData.movementLineUp[1]);
+            piece.row = 5;
+            piece.col = 0;
+            piece.side = 'left';
+            const result = rook.getAttacktNextMov(mockedData.movementLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneLeftMovement);
+        });
+
+        test("should returns one top movement", () => {
+            const piece = decode.fromPieceDecl(mockedData.movementLineUp[1]);
+            piece.row = 7;
+            piece.col = 2;
+            piece.side = 'top';
+            const result = rook.getAttacktNextMov(mockedData.movementLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneTopMovement);
+        })
+
+        test("should not returns any movement or attack", () => {
+            const piece = decode.fromPieceDecl(mockedData.lineupNomovementAttack[1]);
+            piece.row = 7;
+            piece.col = 2;
+            piece.side = 'top';
+            const result = rook.getAttacktNextMov(mockedData.lineupNomovementAttack, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.noAttackAndMove);
+        });
+
+        test("should not returns any movement or attack for a invalid column", () => {
+            const piece = decode.fromPieceDecl(mockedData.movementLineUp[1]);
+            piece.row = 7;
+            piece.col = 9;
+            piece.side = 'top';
+            const result = rook.getAttacktNextMov(mockedData.movementLineUp, piece, endDir);
+            expect(result).toBeNull();
+        });
+    })
+
+    describe("Validate utils functions", () => {
+        test("should returns true when position col is the same of position x ", () => {
+            const position = {
+                x: 1,
+                col: 1
+            };
+            expect(rook.isValidPosition(position)).toBe(true);
+        });
+
+        test("should returns true when position row is the same of position y ", () => {
+            const position = {
+                y: 3,
+                row: 3
+            };
+            expect(rook.isValidPosition(position)).toBe(true);
+        })
+
+        test("should returns false when position col is diff of x and row is diff of y  ", () => {
+            const position = {
+                y: 3,
+                row: 4,
+                x: 1,
+                col: 4
+            };
+            expect(rook.isValidPosition(position)).toBe(false);
+        });
+
+        test("test if it is a valid left position", () => {
+            const position = {
+                side: 'left',
+                col: 2,
+                x: 3
+            };
+
+            expect(rook.isLeftPosition(position, {
+                left: false
+            })).toBe(true);
+            expect(rook.isLeftPosition(position, {
+                left: true
+            })).toBe(false);
+        })
+
+        test("test if it is a valid right position", () => {
+            const position = {
+                side: 'right',
+                col: 4,
+                x: 3
+            };
+
+            expect(rook.isRightPosition(position, {
+                right: false
+            })).toBe(true);
+            expect(rook.isRightPosition(position, {
+                right: true
+            })).toBe(false);
+        });
+
+        test("test if it is a valid top position", () => {
+            const position = {
+                side: 'top',
+                row: 4,
+                y: 3
+            };
+
+            expect(rook.isUpPosition(position, {
+                top: false
+            })).toBe(true);
+            expect(rook.isUpPosition(position, {
+                top: true
+            })).toBe(false);
+        });
+
+        test("test if it is a valid down position", () => {
+            const position = {
+                side: 'down',
+                row: 4,
+                y: 5
+            };
+
+            expect(rook.isDownPosition(position, {
+                down: false
+            })).toBe(true);
+            expect(rook.isDownPosition(position, {
+                down: true
+            })).toBe(false);
+        });
+    });
+
+    describe("generateRookPos", () => {
+        beforeEach(() => {
+            endDir = {
+                left: false,
+                right: false,
+                top: false,
+                down: false
+            };
+        });
+
+        test("should return attacks and movs for the down", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 2;
+            piece.col = 2;
+            piece.side = 'down';
+            const result = rook.generateRookPos(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneDownAttack);
+        })
+
+        test("should return attacks and movs for the right", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 5;
+            piece.col = 4;
+            piece.side = 'right';
+            const result = rook.generateRookPos(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneRightAttack);
+        });
+
+        test("should return attacks and movs for the left", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 5;
+            piece.col = 0;
+            piece.side = 'left';
+            const result = rook.generateRookPos(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneLeftAttack);
+        })
+
+        test("should return attacks and movs for the top", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            piece.row = 7;
+            piece.col = 2;
+            piece.side = 'top';
+            const result = rook.getAttacktNextMov(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result).toEqual(mockedData.oneTopAttack);
+        });
+    });
+
+    describe("Test positions creation by side", () => {
+        beforeEach(() => {
+            endDir = {
+                left: false,
+                right: false,
+                top: false,
+                down: false
+            };
+        });
+
+        test("should return all left moves and attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            const result = rook.getLeftPosition(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(1);
+            expect(result.movs).toHaveLength(1);
+            expect(result).toEqual(mockedData.leftMovsAttacks);
+        });
+
+        test("should return all right moves and attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            const result = rook.getRightPosition(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(1);
+            expect(result.movs).toHaveLength(1);
+            expect(result).toEqual(mockedData.rightMovsAttacks);
+        });
+
+        test("should return all down moves and attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            const result = rook.getDownPosition(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(1);
+            expect(result.movs).toHaveLength(2);
+            expect(result).toEqual(mockedData.downMovsAttacks);
+        });
+
+        test("should return all top moves and attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            const result = rook.getUpPosition(mockedData.defaultLineUp, piece, endDir);
+            expect(result).not.toBeNull();
+            expect(result.attacks).toHaveLength(1);
+            expect(result.movs).toHaveLength(1);
+            expect(result).toEqual(mockedData.topMovsAttacks);
+        });
+    })
+
+    describe("getOptions", () => {
+        test("should generate five moves and four attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            const result = rook.getOptions(mockedData.defaultLineUp, piece);
+            expect(result).not.toBeNull();
+            expect(result.nextMovements).toHaveLength(5);
+            expect(result.attacks).toHaveLength(4);
+            expect(result).toEqual(mockedData.fiveMovesFourAttacks);
+        });
+
+        test("should generate five moves and one attack", () => {
+            const piece = decode.fromPieceDecl(mockedData.defaultLineUp[1]);
+            const result = rook.getOptions(mockedData.defaultLineUp, piece, mockedData.defaultLineUp[2]);
+            expect(result).not.toBeNull();
+            expect(result.nextMovements).toHaveLength(5);
+            expect(result.attacks).toHaveLength(1);
+            expect(result).toEqual(mockedData.fiveMovesOneAttack);
+        });
+
+        test("should generate one move and no attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.getOptions1Mov[1]);
+            const result = rook.getOptions(mockedData.getOptions1Mov, piece);
+            expect(result).not.toBeNull();
+            expect(result.nextMovements).toHaveLength(1);
+            expect(result.attacks).toHaveLength(0);
+            expect(result).toEqual(mockedData.oneMovesNoAttack);
+        })
+
+        test("should generate three moves and no attacks", () => {
+            const piece = decode.fromPieceDecl(mockedData.getOptions3Mov[1]);
+            const result = rook.getOptions(mockedData.getOptions3Mov, piece);
+            expect(result).not.toBeNull();
+            expect(result.nextMovements).toHaveLength(3);
+            expect(result.attacks).toHaveLength(0);
+            expect(result).toEqual(mockedData.threeMovesNoAttack);
+        });
+    });
+});
