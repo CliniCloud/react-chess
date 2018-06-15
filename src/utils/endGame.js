08cdda13a966d21400ba6387e1c8cdf3a8770b8f
aquilesds <aquilesds@clinicloud.com>
2018-06-15 12:21:26 +1000

    Adding improvements and tests to end of game

--- src/utils/endGame.js
+++ src/utils/endGame.js
@@ -1,6 +1,6 @@
-const predictions = require('./predictions')
-const general = require('./general')
-const decode = require('../decode')
+const predictions = require('./predictions');
+const general = require('./general');
+const decode = require('../decode');
 
 /**
  * Check if the game ended in a checkmate
@@ -9,109 +9,77 @@ const decode = require('../decode')
  * @param {string} enemyTeam oposite player
  * @param {object} inCheck object with attrs about the king
  */
-const isCheckmate = (pieces, turn, enemyTeam, isKingThreatened, threateningPos) => {
-    if (isKingThreatened) {
-        const teamPieces = general.getPiecesFromTeam(pieces, turn)
-        const enemyKing = predictions.getKingPos(pieces, enemyTeam)
-        const enemyMovs = []
-
-        for (const piece of teamPieces) {
-            const decodedPiece = general.getPieceObject(piece, null, null)
-            const nextMovsAtt = predictions.getOptionsByName(pieces, decodedPiece, null, null)
-
-            if (nextMovsAtt.nextMovements) {
-                enemyMovs.push({
-                    nextMovements: nextMovsAtt.nextMovements,
-                    name: piece
-                })
+const isCheckmate = (pieces, turn, enemyTeam) => {
+    const teamPieces = general.getPiecesFromTeam(pieces, turn);
+    const enemyKing = predictions.getKingPos(pieces, enemyTeam);
+    const teamMovs = teamPieces.map(piece => {
+        const decodedPiece = general.getPieceObject(piece, null, null);
+        const nextMovsAtt = predictions.getOptionsByName(pieces, decodedPiece, null, null);
+        if (nextMovsAtt.nextMovements && nextMovsAtt.nextMovements.length) {
+            return {
+                nextMovements: nextMovsAtt.nextMovements,
+                name: piece
             }
         }
-
-        const movs = predictions.getOptionsByName(pieces, enemyKing, enemyMovs)
-
-        if(!movs.nextMovements.length && !movs.attacks.length){
-            return true
-        }else{
-            for(const nextMovs of movs.nextMovements){
-                if(!predictions.willTheKingBeThreating(pieces, enemyKing, nextMovs).isKingThreatened){
-                    return false
-                }
-            }
-            for(const attack of movs.attacks){
-                if(!predictions.willTheKingBeThreating(pieces, enemyKing, attack).isKingThreatened){
-                    return false
-                }
-            }
-        }
-
-        return true    
-        
+        return false
+    }).filter(Boolean);
+    
+    const enemyKingMovs = predictions.getOptionsByName(pieces, enemyKing, teamMovs);
+    const noThreatenedMov = enemyKingMovs.nextMovements.find(nextMovs => predictions.willTheKingBeThreating(pieces, enemyKing, nextMovs).isKingThreatened === false)
+    if (noThreatenedMov) {
+        return false;
     }
 
-    return false
-}
+    const noThreatenedAttack = enemyKingMovs.attacks.find(attack => predictions.willTheKingBeThreating(pieces, enemyKing, attack).isKingThreatened === false)
+    if (noThreatenedAttack) {
+        return false;
+    }
+
+    return true;
+};
 
 /**
  * Check if the game ended in a stalemate
  * @param {array} pieces all pieces in the table
  * @param {string} turn player playing
  * @param {string} enemyTeam oposite player
- * @param {object} inCheck object with attrs about the king 
  */
-const isStalemate = (pieces, turn, enemyTeam, isKingThreatened) => {
-    if (isKingThreatened) {
-        return false
-    }
-
+const isStalemate = (pieces, turn, enemyTeam) => {
     const enemyNextMovs = predictions.getEnemyNextMovs(pieces, enemyTeam)
-    const teamPieces = general.getPiecesFromTeam(pieces, enemyTeam)
+    const teamPieces = general.getPiecesFromTeam(pieces, enemyTeam);
     for (const piece of teamPieces) {
-        const decodedPiece = decode.fromPieceDecl(piece)
-        const result = predictions.getOptionsByName(pieces, decodedPiece, enemyNextMovs)
-
-        if(result.nextMovements.length === 0){
-            for(const attack of result.attacks){
-                if(!predictions.willTheKingBeThreating(pieces, decodedPiece, attack).isKingThreatened){
-                    return false
-                }
-            }
-        }else{
-            return false
+        const decodedPiece = decode.fromPieceDecl(piece);
+        const result = predictions.getOptionsByName(pieces, decodedPiece, enemyNextMovs);
+        const noThreatenedMov = result.nextMovements.find(movement => predictions.willTheKingBeThreating(pieces, decodedPiece, movement).isKingThreatened === false)
+        if (noThreatenedMov) {
+            return false;
+        }
+        
+        const noThreatenedAttack = result.attacks.find(attack => predictions.willTheKingBeThreating(pieces, decodedPiece, attack).isKingThreatened === false);
+        if (noThreatenedAttack) {
+            return false;
         }
     }
 
-    return true
-
+    return true;
 }
 
-
-const getPieceByName = (pieces, name) => {
-    for (const piece of pieces) {
-        if (piece[0] === name) {
-            return piece
-        }
-    }
-
-    return null
-}
+/**
+ * Get piece by name
+ * @param {array} pieces array of string pieces
+ * @param {string} name piece name
+ */
+const getPieceByName = (pieces, name) => pieces.find(piece => piece[0] === name);
 
 /**
  * Check if it still possible any player win with a checkmate
  * @param {array} pieces all pieces in the table
  */
-const isimpossibleCheckmate = pieces => {
-    if (pieces.length === 2) {
-        return true
-    } else if (pieces.length === 3 && (getPieceByName(pieces, 'B') || getPieceByName(pieces, 'b'))) {
-        return true
-    } else if (pieces.length === 3 && (getPieceByName(pieces, 'K') || getPieceByName(pieces, 'k'))) {
-        return true
-    } else if (pieces.length <= 4 && getPieceByName(pieces, 'B') && getPieceByName(pieces, 'b')) {
-        return true
-    }
+const isImpossibleCheckmate = pieces =>
+        (pieces.length === 2)
+        || ( pieces.length === 3 && ( !!getPieceByName(pieces, 'B') || !!getPieceByName(pieces, 'b') || !!getPieceByName(pieces, 'N') || !!getPieceByName(pieces, 'n') ) )
+        || ( pieces.length === 4 && !!getPieceByName(pieces, 'B') && !!getPieceByName(pieces, 'b') );
 
-    return false
-}
 
 /**
  * Check if the game ended
@@ -119,27 +87,38 @@ const isimpossibleCheckmate = pieces => {
  * @param {string} turn player playing
  */
 const isGameFinished = (pieces, turn, check) => {
-    const enemyTeam = turn === 'W' ? 'B' : 'W'
-    const inCheck = check ? check : predictions.isEnemyThreateningKing(pieces, enemyTeam)
-    let checkmate = false
-    let stalemate = false
-    let impossibleCheckmate = false
+    const enemyTeam = turn === 'W' ? 'B' : 'W';
+    const inCheck = check ? check : predictions.getThreatenedKingData(pieces, enemyTeam);
+    let checkmate = false;
+    let stalemate = false;
+    let impossibleCheckmate = false;
 
-    if (isCheckmate(pieces, turn, enemyTeam, inCheck.isKingThreatened, inCheck.threateningPos)) {
-        checkmate = true
-    } else if (isStalemate(pieces, turn, enemyTeam, inCheck.isKingThreatened)) {
-        stalemate = true
-    } else if (isimpossibleCheckmate(pieces)) {
-        impossibleCheckmate = true
+    if (inCheck.isKingThreatened && isCheckmate(pieces, turn, enemyTeam)) {
+        checkmate = true;
+    } else if (!inCheck.isKingThreatened && isStalemate(pieces, turn, enemyTeam)) {
+        stalemate = true;
+    } else if (isImpossibleCheckmate(pieces)) {
+        impossibleCheckmate = true;
     }
 
     return {
         isCheckmate: checkmate,
         isStalemate: stalemate,
-        isimpossibleCheckmate: impossibleCheckmate
-    }
+        isImpossibleCheckmate: impossibleCheckmate
+    };
 }
 
-module.exports = {
+let API = {
     isGameFinished
+};
+
+if (general.isTestEnv()) {
+    API = Object.assign({}, API, {
+        isCheckmate,
+        isStalemate,
+        getPieceByName,
+        isImpossibleCheckmate,
+    });
 }
+
+module.exports = API;
---
+++ test/utils/endGame.test.js
@@ -0,0 +1,248 @@
+const endGame = require('../../src/utils/endGame');
+const predictions = require('../../src/utils/predictions');
+const mockedData = require('./endGameMockData.json');
+
+describe('EndGame', () => {
+    describe('isCheckmate', () => {
+        test('should return true when king was in checkmate position 1', () => {
+            expect(endGame.isCheckmate(mockedData.isCheckmateLineup1, 'W', 'B')).toEqual(true);
+        });
+        
+        test('should return true when king was in checkmate position 2', () => {
+            expect(endGame.isCheckmate(mockedData.isCheckmateLineup2, 'W', 'B')).toEqual(true);
+        });
+        
+        test('should return true when king was in checkmate position 4', () => {
+            expect(endGame.isCheckmate(mockedData.isCheckmateLineup3, 'W', 'B')).toEqual(true);
+        });
+        
+        test('should return true when king was in checkmate position 5', () => {
+            expect(endGame.isCheckmate(mockedData.isCheckmateLineup4, 'W', 'B')).toEqual(true);
+        });
+        
+        test('should return true when king was in checkmate position 6', () => {
+            expect(endGame.isCheckmate(mockedData.isCheckmateLineup7, 'W', 'B')).toEqual(false);
+        });
+        
+        test('should return true when king was in checkmate position 7', () => {
+            expect(endGame.isCheckmate(mockedData.isCheckmateLineup8, 'W', 'B')).toEqual(false);
+        });
+    });
+
+    describe('isStalemate', () => {
+        test('should return true when king was stalemate position 1', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup1, 'W', 'B')).toEqual(true);
+        });
+
+        test('should return true when king was stalemate position 2', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup4, 'W', 'B')).toEqual(true);
+        });
+
+        test('should return true when king was stalemate position 3', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup5, 'W', 'B')).toEqual(true);
+        });
+
+        test('should return true when king was stalemate position 4', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup6, 'W', 'B')).toEqual(true);
+        });
+
+        test('should return true when king was stalemate position 5', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup7, 'W', 'B')).toEqual(true);
+        });
+
+        test('should return false when king has space to move', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup2, 'W', 'B')).toEqual(false);
+        });
+
+        test('should return false when same team piece blocks kigns attackers', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup3, 'W', 'B')).toEqual(false);
+        });
+
+        test('should return false when same enemy piece blocks kings attackers', () => {
+            expect(endGame.isStalemate(mockedData.isStalemateLineup8, 'W', 'B')).toEqual(false);
+        });
+    });
+    
+    describe('getPieceByName', () => {
+        test("should return 'Q-0@h7' when sent 'Q'", () =>{
+            expect(endGame.getPieceByName(mockedData.getPieceByNameLineup, 'Q')).toEqual('Q-0@h7');
+        });
+
+        test("should return 'R-0@g8' when sent 'R'", () =>{
+            expect(endGame.getPieceByName(mockedData.getPieceByNameLineup, 'R')).toEqual('R-0@g8');
+        });
+
+        test("should return 'K-0@a5' when sent 'K'", () =>{
+            expect(endGame.getPieceByName(mockedData.getPieceByNameLineup, 'K')).toEqual('K-0@a5');
+        });
+
+        test("should return 'k-0@e8' when sent 'k'", () =>{
+            expect(endGame.getPieceByName(mockedData.getPieceByNameLineup, 'k')).toEqual('k-0@e8');
+        });
+
+        test("should return undefined when sent 'r'", () =>{
+            expect(endGame.getPieceByName(mockedData.getPieceByNameLineup, 'r')).toBeUndefined();
+        });
+
+        test("should return undefined when sent 'q'", () =>{
+            expect(endGame.getPieceByName(mockedData.getPieceByNameLineup, 'q')).toBeUndefined();
+        });
+    });
+
+    describe('isImpossibleCheckmate', () => {
+        test('should return true when there are one white king vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup0)).toEqual(true);
+        });
+
+        test('should return true when there are one king and one bishop white in vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup1)).toEqual(true);
+        });
+        
+        test('should return true when there are one king and one bishop black vs a white king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup2)).toEqual(true);
+        });
+        
+        test('should return true when there are one king and one knight white vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup3)).toEqual(true);
+        });
+        
+        test('should return true when there are one king and one knight black vs a white king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup4)).toEqual(true);
+        });
+        
+        test('should return false when there are one king and one pawn white vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup5)).toEqual(false);
+        });
+        
+        test('should return false when there are one king and one pawn black vs a white king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup6)).toEqual(false);
+        });
+        
+        test('should return false when there are one king and two bishops white vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup7)).toEqual(false);
+        });
+        
+        test('should return false when there are one king and two bishops black  vs a white king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup8)).toEqual(false);
+        });
+        
+        test('should return false when there are one king and two knight white vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup9)).toEqual(false);
+        });
+        
+        test('should return false when there are one king and two knight black  vs a white king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup10)).toEqual(false);
+        });
+        
+        test('should return false when there are one king, one bishof and one knight white vs a black king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup11)).toEqual(false);
+        });
+        
+        test('should return false when there are one king, one bishop and two knight black vs a white king', () => {
+            expect(endGame.isImpossibleCheckmate(mockedData.isImpossibleCheckmateLineup12)).toEqual(false);
+        });
+    });
+
+    describe('isGameFinished', () => {
+        test('should return stalemate when the game is stalemate 1', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup1, 'W')).toEqual(mockedData.isGameFinishedStalemate);
+        });
+
+        test('should return stalemate when the game is stalemate 2', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup4, 'W')).toEqual(mockedData.isGameFinishedStalemate);
+        });
+
+        test('should return stalemate when the game is stalemate 3', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup5, 'W')).toEqual(mockedData.isGameFinishedStalemate);
+        });
+
+        test('should return stalemate when the game is stalemate 4', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup6, 'W')).toEqual(mockedData.isGameFinishedStalemate);
+        });
+
+        test('should return stalemate when the game is stalemate 5', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup7, 'W')).toEqual(mockedData.isGameFinishedStalemate);
+        });
+
+        test('should return checkmate when the game is checkmate 1', () => {
+            expect(endGame.isGameFinished(mockedData.isCheckmateLineup1, 'W')).toEqual(mockedData.isGameFinishedCheckmate);
+        });
+
+        test('should return checkmate when the game is checkmate 2', () => {
+            expect(endGame.isGameFinished(mockedData.isCheckmateLineup2, 'W')).toEqual(mockedData.isGameFinishedCheckmate);
+        });
+
+        test('should return checkmate when the game is checkmate 3', () => {
+            expect(endGame.isGameFinished(mockedData.isCheckmateLineup3, 'W')).toEqual(mockedData.isGameFinishedCheckmate);
+        });
+
+        test('should return checkmate when the game is checkmate 4', () => {
+            expect(endGame.isGameFinished(mockedData.isCheckmateLineup4, 'W')).toEqual(mockedData.isGameFinishedCheckmate);
+        });
+
+        test('should return draw when the game is draw 1', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup0, 'W')).toEqual(mockedData.isGameFinishedDraw);
+        });
+
+        test('should return draw when the game is draw 2', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup1, 'W')).toEqual(mockedData.isGameFinishedDraw);
+        });
+
+        test('should return draw when the game is draw 3', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup2, 'W')).toEqual(mockedData.isGameFinishedDraw);
+        });
+
+        test('should return draw when the game is draw 4', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup3, 'W')).toEqual(mockedData.isGameFinishedDraw);
+        });
+
+        test('should return draw when the game is draw 5', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup4, 'W')).toEqual(mockedData.isGameFinishedDraw);
+        });
+
+        test('should return game is not finished when position 1', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup2, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 2', () => {
+            expect(endGame.isGameFinished(mockedData.isStalemateLineup3, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 3', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup5, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 4', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup6, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 5', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup7, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 6', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup8, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 7', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup9, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 8', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup10, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 9', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup11, 'W')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 10', () => {
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup13, 'B')).toEqual(mockedData.isGameFinishedFalse);
+        });
+
+        test('should return game is not finished when position 11', () => {
+            const check = predictions.getThreatenedKingData(mockedData.isImpossibleCheckmateLineup13, 'W');
+            expect(endGame.isGameFinished(mockedData.isImpossibleCheckmateLineup13, 'B', check)).toEqual(mockedData.isGameFinishedFalse);
+        });
+    });
+});
