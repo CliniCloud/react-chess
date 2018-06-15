70ee10cbc0691cb1e57509030cae9702fe9c90ea
aquilesds <aquilesds@clinicloud.com>
2018-06-15 12:20:17 +1000

    Adding comments and tests to predictions

--- src/utils/predictions.js
+++ src/utils/predictions.js
@@ -1,11 +1,11 @@
-const decode = require('../decode')
-const general = require('./general')
-const bishop = require('./bishop')
-const rook = require('./rook')
-const knight = require('./knight')
-const pawn = require('./pawn')
-const king = require('./king')
-const queen = require('./queen')
+const decode = require('../decode');
+const general = require('./general');
+const bishop = require('./bishop');
+const rook = require('./rook');
+const knight = require('./knight');
+const pawn = require('./pawn');
+const king = require('./king');
+const queen = require('./queen');
 
 /**
  * Get next movements and attacks by piece
@@ -15,22 +15,22 @@ const queen = require('./queen')
  * @param {object} threateningPos posistion of enemy threatening
  */
 const getOptionsByName = (pieces, piece, enemysPossibleMov, threateningPos) => {
-    const upperName = piece.name.toUpperCase()
+    const upperName = piece.name.toUpperCase();
     switch (upperName) {
         case 'P':
-            return pawn.getOptions(pieces, piece, threateningPos)
+            return pawn.getOptions(pieces, piece, threateningPos);
         case 'N':
-            return knight.getOptions(pieces, piece, threateningPos)
+            return knight.getOptions(pieces, piece, threateningPos);
         case 'R':
-            return rook.getOptions(pieces, piece, threateningPos)
+            return rook.getOptions(pieces, piece, threateningPos);
         case 'K':
-                return king.getOptions(pieces, piece, enemysPossibleMov)
+            return king.getOptions(pieces, piece, enemysPossibleMov, threateningPos);
         case 'B':
-            return bishop.getOptions(pieces, piece, threateningPos)
+            return bishop.getOptions(pieces, piece, threateningPos);
         case 'Q':
-            return queen.getOptions(pieces, piece, threateningPos)
+            return queen.getOptions(pieces, piece, threateningPos);
         default:
-            return null
+            return null;
     }
 }
 
@@ -40,26 +40,60 @@ const getOptionsByName = (pieces, piece, enemysPossibleMov, threateningPos) => {
  * @param {string} team current team
  */
 const getKingPos = (pieces, team) => {
-    let kingPos
+    const kingPos = pieces.find(piece =>
+        (team === 'B' && piece[0] === piece[0].toLowerCase() && piece[0] === 'k') ||
+        (team === 'W' && piece[0] === piece[0].toUpperCase() && piece[0] === 'K'));
+
+    if (kingPos) {
+        return Object.assign({}, {
+                notation: kingPos
+            },
+            decode.fromPieceDecl(kingPos)
+        );
+    }
+
+    return false;
+}
+
+/**
+ * Get enemy's next movements, attacks and attackers positions
+ * @param {array} pieces string array of pieces
+ * @param {string} currTeam current team
+ */
+const getEnemyPositions = (pieces, currTeam) => {
+    const enemysPossibleMov = [];
+    const enemysPossibleAttacks = [];
+    const attackerPositions = [];
     for (const piece of pieces) {
-        if (piece[0].toUpperCase() === 'K') {
-            if (team === 'B' && piece[0] === piece[0].toLowerCase()) {
-                kingPos = piece
-                break
-            } else if (team === 'W' && piece[0] === piece[0].toUpperCase()) {
-                kingPos = piece
-                break
+        let piecePos;
+        let pieceName;
+        if (currTeam === 'B' && piece[0] === piece[0].toUpperCase()) {
+            piecePos = piece;
+            pieceName = piece.substr(0, 1);
+        } else if (currTeam === 'W' && piece[0] === piece[0].toLowerCase()) {
+            piecePos = piece;
+            pieceName = piece.substr(0, 1);
+        }
+
+        if (piecePos && pieceName) {
+            const pieceObj = general.findPieceAtPosition(pieces, piecePos);
+            const result = getOptionsByName(pieces, pieceObj, null);
+            enemysPossibleMov.push({
+                nextMovements: result.nextMovements,
+                name: pieceObj.name
+            });
+            if (result.attacks.length) {
+                enemysPossibleAttacks.push(result.attacks);
             }
+            attackerPositions.push(piecePos);
         }
     }
 
-    if (kingPos) {
-        return Object.assign({
-            notation: kingPos
-        }, decode.fromPieceDecl(kingPos))
-    }
-
-    return false
+    return {
+        enemysPossibleMov,
+        enemysPossibleAttacks,
+        attackerPositions
+    };
 }
 
 /**
@@ -68,43 +102,22 @@ const getKingPos = (pieces, team) => {
  * @param {array} pieces string array of pieces
  * @param {string} currTeam current team
  */
-const isEnemyThreateningKing = (pieces, currTeam) => {
-    const enemysPossibleMov = []
-    const enemysPossibleAttacks = []
-    const attackerPositions = []
-    for (const piece of pieces) {
-        let piecePos
-        let pieceName
-        if (currTeam === 'B' && piece[0] === piece[0].toUpperCase()) {
-            piecePos = piece
-            pieceName = piece.substr(0, 1)
-        } else if (currTeam === 'W' && piece[0] === piece[0].toLowerCase()) {
-            piecePos = piece
-            pieceName = piece.substr(0, 1)
-        }
-
-        if (piecePos && pieceName) {
-            const pieceObj = general.findPieceAtPosition(pieces, piecePos)
-            const result = getOptionsByName(pieces, pieceObj, null)
-            enemysPossibleMov.push({
-                nextMovements: result.nextMovements,
-                name: pieceObj.name
-            })
-            enemysPossibleAttacks.push(result.attacks)
-            attackerPositions.push(piecePos)
-        }
-    }
+const getThreatenedKingData = (pieces, currTeam) => {
+    const enemysPos = getEnemyPositions(pieces, currTeam);
+    const enemysPossibleMov = enemysPos.enemysPossibleMov;
+    const enemysPossibleAttacks = enemysPos.enemysPossibleAttacks;
+    const attackerPositions = enemysPos.attackerPositions;
 
     const kingPos = getKingPos(pieces, currTeam)
-    let isKingThreatened = false
-    let threateningPos = null
-    for(let index = 0; index < enemysPossibleAttacks.length; index++) {
-        const attacks = enemysPossibleAttacks[index]
+    let isKingThreatened = false;
+    let threateningPos = null;
+    for (let index = 0; index < enemysPossibleAttacks.length; index++) {
+        const attacks = enemysPossibleAttacks[index];
         for (const attack of attacks) {
             if (attack.x === kingPos.x && attack.y === kingPos.y) {
-                isKingThreatened = true
-                threateningPos = attackerPositions[index]
-                break
+                isKingThreatened = true;
+                threateningPos = attackerPositions[index];
+                break;
             }
         }
     }
@@ -114,7 +127,7 @@ const isEnemyThreateningKing = (pieces, currTeam) => {
         threatenedKingPos: isKingThreatened ? kingPos : null,
         enemysPossibleMov: enemysPossibleMov,
         threateningPos
-    }
+    };
 }
 
 /**
@@ -124,59 +137,72 @@ const isEnemyThreateningKing = (pieces, currTeam) => {
  * @param {object} newPosition piece new position object
  */
 const willTheKingBeThreating = (pieces, piece, newPosition) => {
-    let replacedEnemy = false
-    let kinsIsDead = false
-    const newMovPos = decode.fromPosDecl(newPosition.x, newPosition.y)
-    const newPieces = pieces.map(( item, index) => {
-        const pos = item.substr(item.indexOf('@') + 1)    
-        if(piece.position === pos){
-            return false
-        }else if(pos === newMovPos){
-            replacedEnemy = true
-            kinsIsDead = item.toUpperCase().indexOf('K') > -1
-            console.log('piece', piece);
-            return `${piece.notation.substr(0, item.indexOf('@'))}@${pos}`
+    let replacedEnemy = false;
+    let kinsIsDead = false;
+    const newMovPos = decode.fromPosDecl(newPosition.x, newPosition.y);
+    const newPieces = pieces.map((item, index) => {
+        const pos = item.substr(item.indexOf('@') + 1);
+        if (piece.position === pos) {
+            return false;
+        } else if (pos === newMovPos) {
+            replacedEnemy = true;
+            kinsIsDead = item.toUpperCase().indexOf('K') > -1;
+            return `${piece.notation.substr(0, item.indexOf('@'))}@${pos}`;
         }
-        return item
-    }).filter(Boolean)
+        return item;
+    }).filter(Boolean);
 
-    if(!replacedEnemy){
-        newPieces.push(`${piece.name}-0@${newMovPos}`)
+    if (!replacedEnemy) {
+        newPieces.push(`${piece.name}-0@${newMovPos}`);
     }
 
-    let enemyThreat = isEnemyThreateningKing(newPieces, general.whatIsMyTeam(piece.name))
-    enemyThreat = Object.assign(enemyThreat, {kinsIsDead})
-
-    return  enemyThreat
+    let enemyThreat = getThreatenedKingData(newPieces, general.whatIsMyTeam(piece.name));
+    enemyThreat = Object.assign({}, enemyThreat, {
+        kinsIsDead
+    });
+    return enemyThreat;
 }
 
-const getEnemyNextMovs = (pieces, enemyTeamName)=> {
-    const movs = []
+/**
+ * Get enemies next allowed movements
+ * @param {array} pieces string array piece
+ * @param {string} enemyTeamName enemy team name ('B' or 'W')
+ */
+const getEnemyNextMovs = (pieces, enemyTeamName) => {
+    const movs = [];
     for (const piece of pieces) {
-        let shouldAddPiece = false
+        let shouldAddPiece = false;
         if (enemyTeamName === 'B' && piece[0] === piece[0].toUpperCase()) {
-            shouldAddPiece = true
+            shouldAddPiece = true;
         } else if (enemyTeamName === 'W' && piece[0] === piece[0].toLowerCase()) {
-            shouldAddPiece = true
+            shouldAddPiece = true;
         }
 
         if (shouldAddPiece) {
-            const  decodedPiece = decode.fromPieceDecl(piece)
-            const result = getOptionsByName(pieces, decodedPiece)
+            const decodedPiece = decode.fromPieceDecl(piece);
+            const result = getOptionsByName(pieces, decodedPiece);
             movs.push({
-                nextMovements:result.nextMovements,
+                nextMovements: result.nextMovements,
                 name: decodedPiece.name
-            })
+            });
         }
     }
 
-    return movs
+    return movs;
 }
 
-module.exports = {
+let API = {
     getOptionsByName,
     getKingPos,
-    isEnemyThreateningKing,
+    getThreatenedKingData,
     willTheKingBeThreating,
     getEnemyNextMovs
 }
+
+if (general.isTestEnv()) {
+    API = Object.assign({}, API, {
+        getEnemyPositions
+    });
+}
+
+module.exports = API;
---
+++ test/utils/predictions.test.js
@@ -0,0 +1,213 @@
+const predictions = require('../../src/utils/predictions');
+const decode = require('../../src/decode');
+const predictionsMockData = require('./predictionsMockData');
+const pawnMockData = require('./pawnMockData');
+const bishopMockData = require('./bishopMockData');
+const knightMockData = require('./knightMockData');
+const rookMockData = require('./rookMockData');
+
+describe('predictions', () => {
+    describe('getOptionsByName', () => {
+        test('should return pawn next movement and attack', () => {
+            const piece = decode.fromPieceDecl(pawnMockData.getOptionsWhitePawn1Att1Mov[1]);
+            expect(predictions.getOptionsByName(pawnMockData.getOptionsWhitePawn1Att1Mov, piece))
+            .toEqual(pawnMockData.whitePawn1Att1MovResult);
+        });
+
+        test('should return pawn next attack again kings threatening', () => {
+            const piece = decode.fromPieceDecl(pawnMockData.getOptionsWhitePawnKingThreathening[1]);
+            expect(predictions.getOptionsByName(
+                pawnMockData.getOptionsWhitePawnKingThreathening,
+                piece,
+                null,
+                pawnMockData.getOptionsWhitePawnKingThreathening[3]))
+            .toEqual(pawnMockData.whitePawn1Att1MovResult);
+        });
+
+        test('should return bishop three movements and one attack', () => {
+            const piece = decode.fromPieceDecl(bishopMockData.oneMovLineup[1]);
+            expect(predictions.getOptionsByName(bishopMockData.oneMovLineup, piece))
+            .toEqual(bishopMockData.threeMovOneAttResult);
+        });
+
+        test('should return bishop attack to king threatening enemy', () => {
+            const piece = decode.fromPieceDecl(bishopMockData.threateningKingLineup[1]);
+            expect(predictions.getOptionsByName(
+                bishopMockData.threateningKingLineup,
+                piece,
+                null,
+                bishopMockData.threateningKingLineup[8]))
+            .toEqual(bishopMockData.threateningKingResult);
+        });
+
+        test('should return eight knight attacks and no movements', () => {
+            const piece = decode.fromPieceDecl(knightMockData.attacksLineUp[2]);
+            expect(predictions.getOptionsByName(knightMockData.attacksLineUp, piece))
+            .toEqual(knightMockData.eightAttacks);
+        });
+
+        test('should return one knight attack to king threatening', () => {
+            const piece = decode.fromPieceDecl(knightMockData.getOptions1Att2KingThreat[2]);
+            expect(predictions.getOptionsByName(knightMockData.getOptions1Att2KingThreat,
+                piece,
+                null,
+                knightMockData.getOptions1Att2KingThreat[7]
+            ))
+            .toEqual(knightMockData.oneAttackNoMovs);
+        });
+
+        test('should return four rook attacks and five movements', () => {
+            const piece = decode.fromPieceDecl(rookMockData.defaultLineUp[1]);
+            expect(predictions.getOptionsByName(rookMockData.defaultLineUp, piece))
+            .toEqual(rookMockData.fiveMovesFourAttacks);
+        });
+
+        test('should return one rook attack and five movements', () => {
+            const piece = decode.fromPieceDecl(rookMockData.defaultLineUp[1]);
+            expect(predictions.getOptionsByName(
+                rookMockData.defaultLineUp,
+                piece,
+                null,
+                rookMockData.defaultLineUp[2]))
+            .toEqual(rookMockData.fiveMovesOneAttack);
+        });
+
+        test('should return null when no piece name is setted', () => {
+            const piece = decode.fromPieceDecl(rookMockData.defaultLineUp[1]);
+            piece.name = '';
+            expect(predictions.getOptionsByName(
+                rookMockData.defaultLineUp,
+                piece,
+                null,
+                rookMockData.defaultLineUp[2]))
+            .toBeNull();
+        });
+
+        test('should return null when a wrong piece name is setted', () => {
+            const piece = decode.fromPieceDecl(rookMockData.defaultLineUp[1]);
+            piece.name = 'X';
+            expect(predictions.getOptionsByName(
+                rookMockData.defaultLineUp,
+                piece,
+                null,
+                rookMockData.defaultLineUp[2]))
+            .toBeNull();
+        });
+
+    });
+
+    describe('getKingPos', () => {
+        test('should return white king position', () => {
+            const piece = decode.fromPieceDecl(predictionsMockData.getKingPosLineup[0]);
+            expect(predictions.getKingPos(predictionsMockData.getKingPosLineup, 'W')).toEqual(piece);
+        });
+
+        test('should return black king position', () => {
+            const piece = decode.fromPieceDecl(predictionsMockData.getKingPosLineup[6]);
+            expect(predictions.getKingPos(predictionsMockData.getKingPosLineup, 'B')).toEqual(piece);
+        });
+
+        test('should return false when no team was sent', () => {
+            expect(predictions.getKingPos(predictionsMockData.getKingPosLineup)).toEqual(false);
+        });
+
+        test('should return false when wrong team was sent', () => {
+            expect(predictions.getKingPos(predictionsMockData.getKingPosLineup, 'V')).toEqual(false);
+        });
+    });
+
+    describe('getEnemyPositions', () => {
+        test('should return black team position and attacks when lineup 1', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup, 'W'))
+            .toEqual(predictionsMockData.getEnemyPositionsResult1);
+        });
+
+        test('should return white team position and attacks when lineup 1', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup, 'B'))
+            .toEqual(predictionsMockData.getEnemyPositionsResult2);
+        });
+
+        test('should return black team position and attacks when lineup 2', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup1, 'W'))
+            .toEqual(predictionsMockData.getEnemyPositionsResult3);
+        });
+
+        test('should return white team position and attacks when lineup 2', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup1, 'B'))
+            .toEqual(predictionsMockData.getEnemyPositionsResult4);
+        });
+    });
+
+    describe('getThreatenedKingData', () => {
+        test('should return white ', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup, 'W'))
+            .toEqual(predictionsMockData.isEnemyThreateningKingResult1);
+        });
+
+        test('should return black ', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup, 'B'))
+            .toEqual(predictionsMockData.isEnemyThreateningKingResult2);
+        });
+
+        test('should return white when lineup2', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup1, 'W'))
+            .toEqual(predictionsMockData.isEnemyThreateningKingResult3);
+        });
+
+        test('should return black when lineup2', () => {
+            expect(predictions.getEnemyPositions(predictionsMockData.getKingPosLineup1, 'B'))
+            .toEqual(predictionsMockData.isEnemyThreateningKingResult4);
+        });
+    });
+    
+    describe('willTheKingBeThreating', () => {
+        test('should king be threatened when same team piece is blocking an attacker and leave the position', () => {
+            const piece = decode.fromPieceDecl(predictionsMockData.willTheKingBeThreatingLineup[5]);
+            expect(predictions.willTheKingBeThreating(
+                predictionsMockData.willTheKingBeThreatingLineup,
+                piece,
+                {x:5, y: 4})
+            ).toEqual(predictionsMockData.willTheKingBeThreatingResult1);
+        });
+
+        test('should king not be threatened when same team piece is not blocking an attacker and leave the position', () => {
+            const piece = decode.fromPieceDecl(predictionsMockData.willTheKingBeThreatingLineup[3]);
+            expect(predictions.willTheKingBeThreating(
+                predictionsMockData.willTheKingBeThreatingLineup,
+                piece,
+                {x:6, y: 1})
+            ).toEqual(predictionsMockData.willTheKingBeThreatingResult2);
+        }); 
+
+        test('should king not be threatened when same team piece is blocking an attacker and attack it', () => {
+            const piece = decode.fromPieceDecl(predictionsMockData.willTheKingBeThreatingLineup2[5]);
+            expect(predictions.willTheKingBeThreating(
+                predictionsMockData.willTheKingBeThreatingLineup2,
+                piece,
+                {x:7, y: 1})
+            ).toEqual(predictionsMockData.willTheKingBeThreatingResult3);
+        }); 
+    });
+
+    describe('getEnemyNextMovs', () => {
+        test('should return black team next movements when using lineup 1', () => {
+            expect(predictions.getEnemyNextMovs(predictionsMockData.getKingPosLineup, 'B'))
+            .toEqual(predictionsMockData.getEnemyNextMovsResult1);
+        }); 
+
+        test('should return white team next movements when using lineup 1', () => {
+            expect(predictions.getEnemyNextMovs(predictionsMockData.getKingPosLineup, 'W'))
+            .toEqual(predictionsMockData.getEnemyNextMovsResult2);
+        });     
+
+        test('should return black team next movements when using lineup 2', () => {
+            expect(predictions.getEnemyNextMovs(predictionsMockData.getKingPosLineup1, 'B'))
+            .toEqual(predictionsMockData.getEnemyNextMovsResult3);
+        });
+
+        test('should return white team next movements when using lineup 2', () => {
+            expect(predictions.getEnemyNextMovs(predictionsMockData.getKingPosLineup1, 'W'))
+            .toEqual(predictionsMockData.getEnemyNextMovsResult4);
+        });
+    });
+});
