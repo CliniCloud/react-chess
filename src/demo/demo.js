24e7a91ee613236d15dadad5736c93cec8e493c2
aquilesds <aquilesds@clinicloud.com>
2018-06-15 12:23:45 +1000

    Fix indentation

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,62 +1,59 @@
-const React = require('react')
-const Chess = require('../../src/react-chess')
+const React = require('react');
+const Chess = require('../../src/react-chess');
 
-require('./demo.css')
+require('./demo.css');
 
 class Demo extends React.PureComponent {
-    constructor(props) {
-        super(props)
-
+    constructor(...args) {
+        super(...args)
         this.state = {
             pieces: Chess.getDefaultLineup()
-        }
-        this.handleMovePiece = this.handleMovePiece.bind(this)
-        this.onPawChooseNewPiece = this.onPawChooseNewPiece.bind(this)
+        };
+        this.handleMovePiece = this.handleMovePiece.bind(this);
+        this.onPawChooseNewPiece = this.onPawChooseNewPiece.bind(this);
     }
 
     handleMovePiece(piece, fromSquare, toSquare, qntPlayed, castlingRookPos) {
-
         const newPieces = this.state.pieces
             .map(curr => {
                 if (curr.substr(curr.indexOf('@') + 1) === fromSquare) {
-                    return `${piece.name}-${qntPlayed}@${toSquare}`
+                    return `${piece.name}-${qntPlayed}@${toSquare}`;
                 } else if (curr.indexOf(toSquare) > -1) {
-                    return false // To be removed from the board
+                    return false; // To be removed from the board
                 }else if(castlingRookPos && curr === castlingRookPos.notation){
-                    return `${castlingRookPos.name}-${castlingRookPos.qntPlayed + 1}@${castlingRookPos.nextPos}`
+                    return `${castlingRookPos.name}-${castlingRookPos.qntPlayed + 1}@${castlingRookPos.nextPos}`;
                 }
-                return curr
+                return curr;
             })
-            .filter(Boolean)
+            .filter(Boolean);
             
         this.setState({
             pieces: newPieces
-        })
+        });
     }
 
     onPawChooseNewPiece(oldPiece, newPiece, newPieceName) {
-
         const newPieces = this.state.pieces
             .map(curr => {
                 if (curr === newPiece) {
-                    return newPiece.replace(newPiece[0], newPieceName)
+                    return newPiece.replace(newPiece[0], newPieceName);
                 }
-                return curr
-            }).filter(Boolean)
+                return curr;
+            }).filter(Boolean);
 
         this.setState({
             pieces: newPieces
-        })
+        });
     }
 
     render() {
-        const {pieces} = this.state
+        const {pieces} = this.state;
         return (
             <div className="demo">
               <Chess pieces={ pieces } onMovePiece={ this.handleMovePiece } onPawChooseNewPiece={ this.onPawChooseNewPiece } />
             </div>
-        )
+        );
     }
 }
 
-module.exports = Demo
+module.exports = Demo;
--- src/piecePositionHoc.js
+++ src/piecePositionHoc.js
@@ -1,16 +1,16 @@
 /* eslint-disable react/display-name, react/prop-types */
-const React = require('react')
+const React = require('react');
 
 module.exports = Piece => props => {
-    const {onMouseDown, onMouseUp, onTouchEnd, onTouchStart, style, isMoving, fill, x, menu, threatened} = props
-    const y = 7 - props.y
-    let styles
+    const {onMouseDown, onMouseUp, onTouchEnd, onTouchStart, style, isMoving, fill, x, menu, threatened} = props;
+    const y = 7 - props.y;
+    let styles;
 
     if (menu) {
         styles = {
             display: 'flex',
             justifyContent: 'center'
-        }
+        };
     } else {
         styles = Object.assign({}, style, {
             position: 'absolute',
@@ -21,23 +21,20 @@ module.exports = Piece => props => {
             textAlign: 'center',
             zIndex: isMoving ? 1000 : undefined,
             boxShadow: threatened ? 'inset 0 0 80px 8px #d85000' : undefined
-        })
-
+        });
     }
-    // { (fill && <Piece size="85%" fill={ fill } />) || <Piece size="85%" /> }
+
     if (fill) {
         return (
             <div onMouseDown={ onMouseDown } onMouseUp={ onMouseUp } onTouchEnd={ onTouchEnd } onTouchStart={ onTouchStart } style={ styles }>
               <Piece size="85%" fill={ fill } />
             </div>
-        )
+        );
     }
 
     return (
         <div onMouseDown={ onMouseDown } onMouseUp={ onMouseUp } onTouchEnd={ onTouchEnd } onTouchStart={ onTouchStart } style={ styles }>
             <Piece size="85%" />
         </div>
-    )
-
-
+    );
 }
