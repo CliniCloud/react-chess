e05f22fbb1e468a9183c0d6625e8c14e4c1b476c
aquilesds <aquilesds@clinicloud.com>
2018-08-20 17:17:10 +1000

    1. Change EndOfGame Component to PureComponent 2. Improve the html indentation

--- src/ui/EndOfGame.js
+++ src/ui/EndOfGame.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-class EndOfGame extends React.Component {
+class EndOfGame extends React.PureComponent {
 
     render(){
         const wrapperStyle = {
@@ -26,9 +26,21 @@ class EndOfGame extends React.Component {
         );
 
         if(this.props.isCheckmate){
-            return endOfGameWrapper(<h2>Checkmate from <span style={{color:this.props.turn === 'B' ? '#d8d8d8' : 'black'}}>{this.props.turn === 'B' ? 'White' : 'Black'}</span></h2>);
+            return endOfGameWrapper(
+              <h2>
+                Checkmate from
+                <span style={{color:this.props.turn === 'B' ? '#d8d8d8' : 'black'}}>
+                  {this.props.turn === 'B' ? 'White' : 'Black'}
+                </span>
+              </h2>);
         }else if(this.props.isStalemate){
-            return endOfGameWrapper(<h2>Stalemate from <span style={{color:this.props.turn === 'B' ? '#d8d8d8' : 'black'}}>{this.props.turn === 'B' ? 'White' : 'Black'}</span></h2>);
+            return endOfGameWrapper(
+              <h2>
+                Stalemate from
+                <span style={{color:this.props.turn === 'B' ? '#d8d8d8' : 'black'}}>
+                  {this.props.turn === 'B' ? 'White' : 'Black'}
+                </span>
+              </h2>);
         }else if(this.props.isImpossibleCheckmate){
             return endOfGameWrapper(<h2>The game has finished in a draw</h2>);
         }
