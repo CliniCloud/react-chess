594a0fee3d061addbf01ca570b4df8932b885283
aquilesds <aquilesds@clinicloud.com>
2018-09-20 17:04:51 +1000

    removing history files

--- .history/src/utils_20180206111955.js
+++
@@ -1,4 +0,0 @@
-module.exports.isPiecesFromSameTeam = function(pieceA, pieceB){
-    return (pieceA.name === pieceA.name.toLowerCase() && pieceB.name === pieceB.name.toLowerCase()) || 
-    (pieceA.name === pieceA.name.toUpperCase() && pieceB.name === pieceB.name.toUpperCase())
-}
